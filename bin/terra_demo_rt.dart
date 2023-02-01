80d6a2992cc8300d90871e32fb840c41a81520c9
Rishav Chatterjee <rishav@tryterra.co>
2023-02-01 15:28:48 +0000

    feat: add init files

---
+++ bin/terra_demo_rt.dart
@@ -0,0 +1,5 @@
+import 'package:terra_demo_rt/terra_demo_rt.dart' as terra_demo_rt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${terra_demo_rt.calculate()}!');
+}
---
+++ test/terra_demo_rt_test.dart
@@ -0,0 +1,8 @@
+import 'package:terra_demo_rt/terra_demo_rt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
