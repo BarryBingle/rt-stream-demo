049e86f89d3e949d0c11b7fc58839bd66cf8ac9c
hanbo-xie <hanbo@tryterra.co>
2023-02-04 03:07:30 +0000

    feat: added prettier linter

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <BrowserRouter>
       <div className="bg-[#F0E9FF] w-screen min-h-screen flex flex-col gap-5 items-center overflow-hidden">
-          <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
